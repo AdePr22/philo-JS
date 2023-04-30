4690b031aca0af671c55d0b263fdf4391091565d
AdePr22 <Adep.pr88@gmail.com>
2023-04-30 16:05:18 +0700

    menambahkan object dengan function declarations dan constructor function

--- object/script.js
+++ object/script.js
@@ -23,3 +23,26 @@ var Teknisi = {
   property: "properties",
 };
 console.log(Teknisi.property);
+
+// Membuat object dengan function declaration
+let objekKaryawan = function (nama, umur, divisi, gaji) {
+  let karya = {};
+  karya.nama = nama;
+  karya.umur = umur;
+  karya.divisi = divisi;
+  karya.gaji = gaji;
+  return karya;
+};
+const karyawan1 = objekKaryawan("thomas", 23, "hrd", 6_000_000);
+console.log(karyawan1.gaji);
+
+// Membuat object dengan constructor fuunction
+function Karyawan(nama, umur, divisi, gaji) {
+  this.nama = nama;
+  this.umur = umur;
+  this.divisi = divisi;
+  this.gaji = gaji;
+}
+let karyawan2 = new Karyawan("antonio", 23, "hrd", 6_000_000);
+console.log(karyawan2.divisi);
+console.log(karyawan2.umur);
